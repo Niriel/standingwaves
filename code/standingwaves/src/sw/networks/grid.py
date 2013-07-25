3a197540abd1907dd7183e1aa90dc00371a9c814
Niriel <daneel_libre@yahoo.fr>
2013-07-25 17:41:53 +0200

    Continued grid code and grid test.  The test shows a problem with transmission in a diplexer configuration of the grid.

--- code/standingwaves/src/sw/networks/grid.py
+++ code/standingwaves/src/sw/networks/grid.py
@@ -9,6 +9,7 @@
 from __future__ import division
 import numpy as np
 import scipy.constants
+import sw.geometry as geo
 
 c0 = scipy.constants.c
 mu0 = scipy.constants.mu_0
@@ -16,7 +17,7 @@ mu0 = scipy.constants.mu_0
 # Insane amount of digits taken from wikipedia.
 euler = 0.57721566490153286060651209008240243104215933593992
 
-def RT(a, d, s, f, propdir):
+def _GridRT(a, d, s, f, propdir):
     kx, ky, kz = propdir
     l = c0 / f  # Wavelength (lambda).
     k = (2 * np.pi) / l  # Wave number.
@@ -25,7 +26,7 @@ def RT(a, d, s, f, propdir):
     Zs = (1 + 1j) * (mu0 * w / (2 * s))
 
     # Terms that appear several times in the equations.
-    Z = Zs / Z0  # For convenience because that ratio shows up a lot.
+    Z = Zs / Z0
     lpdz = l / (np.pi * d * kz)
     lnd2pa = np.log(d / (2 * np.pi * a))  # ln.
     kx21 = (1 - kx ** 2)
@@ -33,8 +34,8 @@ def RT(a, d, s, f, propdir):
     ka2 = k * a / 2
     k1a2 = k1 * a / 2
 
-    Nx = complex(1, Z * ka2)
-    Nc = complex(1, Z / ka2)
+    Nx = 1 + 1j * Z * ka2
+    Nc = 1 + 1j * Z / ka2
 
     Dx = kx21 * (
                     (
@@ -75,9 +76,45 @@ def RT(a, d, s, f, propdir):
     Tzy = +common_c * ky
     Tzz = -common_c * ky * ky / kz + 1
     R = np.array([[Rxx, Rxy, Rxz],
-                      [Ryx, Ryy, Ryz],
-                      [Rzx, Rzy, Rzz]])
+                  [Ryx, Ryy, Ryz],
+                  [Rzx, Rzy, Rzz]])
     T = np.array([[Txx, Txy, Txz],
-                      [Tyx, Tyy, Tyz],
-                      [Tzx, Tzy, Tzz]])
+                  [Tyx, Tyy, Tyz],
+                  [Tzx, Tzy, Tzz]])
     return R, T
+
+def Grid(a, d, s, f, tb, attitude, k1):
+    A = tb.rot(*attitude)
+    At = A.T
+    n = A.dot(tb.rest)  # Normal to the grid.
+    q = geo.RotationAroundAxisQuaternion(n, geo.TAU / 2)
+    k2 = geo.QuatRotate(q, k1)
+    k3 = -k1
+    k4 = -k2
+
+    # It is probably possible to use symmetries in order to compute R and T only
+    # once.  This is a possible optimization that I may consider later if
+    # profiling shows this to be a bottleneck.  Note that computing R and T once
+    # only, then using rotation matrices to generate the other Rs and Ts may
+    # take as long.
+    # The direction of propagations sent to _GridRT must be in the reference
+    # frame of the grid.
+    R1, T1 = _GridRT(a, d, s, f, At.dot(k1))
+    R2, T2 = _GridRT(a, d, s, f, At.dot(k2))
+    R3, T3 = _GridRT(a, d, s, f, At.dot(k3))
+    R4, T4 = _GridRT(a, d, s, f, At.dot(k4))
+    #
+    R1 = A.dot(R1).dot(At)
+    R2 = A.dot(R2).dot(At)
+    R3 = A.dot(R3).dot(At)
+    R4 = A.dot(R4).dot(At)
+    T1 = A.dot(T1).dot(At)
+    T2 = A.dot(T2).dot(At)
+    T3 = A.dot(T3).dot(At)
+    T4 = A.dot(T4).dot(At)
+    ZZ = np.zeros_like(R1)
+    S = np.bmat([[ZZ, R2, T3, ZZ],
+                 [R1, ZZ, ZZ, T4],
+                 [T1, ZZ, ZZ, R4],
+                 [ZZ, T2, R3, ZZ]])
+    return np.array(S)
--- code/standingwaves/src/sw/networks/test_grid.py
+++ code/standingwaves/src/sw/networks/test_grid.py
@@ -5,6 +5,7 @@ Created on Jul 23, 2013
 '''
 import unittest
 import numpy as np
+import sw.geometry as geo
 import grid
 
 class TestRT(unittest.TestCase):
@@ -14,7 +15,7 @@ class TestRT(unittest.TestCase):
         s = 1e10  # Extremely good conductor, perfect.
         f = 1e12  # 1 THz, typical HIFI frequency.
         propdir = np.array([0, 0, 1])  # Normal incidence.
-        R, T = grid.RT(a, d, s, f, propdir)
+        R, T = grid._GridRT(a, d, s, f, propdir)
         # Field parallel to the wires.
         # ----------------------------
         ei = np.array([1, 0, 0])
@@ -43,6 +44,136 @@ class TestRT(unittest.TestCase):
         Pr = np.linalg.norm(er) ** 2
         Pt = np.linalg.norm(et) ** 2
         self.assertTrue(0 <= Pi - Pr - Pt <= 1.e-3)
+    def compare(self, G, a, expected):
+            b = G.dot(a)
+#             print np.abs(b)
+#             print np.abs(expected)
+            self.assertTrue(np.allclose(b, expected, 0, 1.e-3))
+    def testAttitude1(self):
+        a = 1e-9  # Very tiny, perfect.
+        d = 10e-9  # Because a<<d required.
+        s = 1e10  # Extremely good conductor, perfect.
+        f = 1e12  # 1 THz, typical HIFI frequency.
+        tb = geo.TaitBryan(0, 1, 2)
+        # Rotate the grid around its normal so that the wires are y-oriented.
+        attitude = (0, 0, geo.TAU / 4)
+        k1 = np.array([0, 0, 1])
+        G = grid.Grid(a, d, s, f, tb, attitude, k1)
+        # Input x field on port 1.
+        # No reflection on 2, transmission on 3.
+        a = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input x field on port 2.
+        # No reflection on 1, transmission on 4.
+        a = np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+        self.compare(G, a, b)
+        # Input x field on port 3.
+        # No reflection on 4, transmission on 1.
+        a = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+        b = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input x field on port 4.
+        # No reflection on 3, transmission on 2.
+        a = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+        b = np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input y field on port 1.
+        # The reflected wave flips sign.
+        # Reflection on 2, no transmission on 3.
+        a = np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input y field on port 2.
+        # Reflection on 1, no transmission on 4.
+        a = np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input y field on port 3.
+        # Reflection on 4, no transmission on 1.
+        a = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0])
+        self.compare(G, a, b)
+        # Input y field on port 4.
+        # Reflection on 3, no transmission on 2.
+        a = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0])
+        self.compare(G, a, b)
+    def testAttitude2(self):
+        a = 1e-9  # Very tiny, perfect.
+        d = 10e-9  # Because a<<d required.
+        s = 1e10  # Extremely good conductor, perfect.
+        f = 1e12  # 1 THz, typical HIFI frequency.
+        tb = geo.TaitBryan(0, 1, 2)
+        # Two rotations.  The grid is now in the plane yz and the wires along z.
+        attitude = (geo.TAU / 4, 0, geo.TAU / 4)
+        k1 = np.array([0, -1, 0])
+        G = grid.Grid(a, d, s, f, tb, attitude, k1)
+        # Input x field on port 1.
+        # No reflection on 2, transmission on 3.
+        a = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input x field on port 2.
+        # No reflection on 1, transmission on 4.
+        a = np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+        self.compare(G, a, b)
+        # Input x field on port 3.
+        # No reflection on 4, transmission on 1.
+        a = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+        b = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input x field on port 4.
+        # No reflection on 3, transmission on 2.
+        a = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+        b = np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input z field on port 1.
+        # Reflection on 2, no transmission on 3.
+        a = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input z field on port 2.
+        # Reflection on 1, no transmission on 4.
+        a = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+        b = np.array([0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.compare(G, a, b)
+        # Input z field on port 3.
+        # Reflection on 4, no transmission on 1.
+        a = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1])
+        self.compare(G, a, b)
+        # Input z field on port 4.
+        # Reflection on 3, no transmission on 2.
+        a = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0])
+        self.compare(G, a, b)
+    def testAttitude3(self):
+        # Diplexer beam splitter configuration. The angle of incidence is 45
+        # degrees. In addition, the wires APPEAR at 45 degrees relatively to the
+        # polarization of the wave.
+        a = 1e-9  # Very tiny, perfect.
+        d = 10e-9  # Because a<<d required.
+        s = 1e10  # Extremely good conductor, perfect.
+        f = 1e12  # 1 THz, typical HIFI frequency.
+        tb = geo.TaitBryan(0, 1, 2)
+        attitude = (geo.TAU / 8, 0, np.arctan(np.sqrt(2)))
+        k1 = np.array([0, 0, 1])
+        G = grid.Grid(a, d, s, f, tb, attitude, k1)
+        # I send x.
+        # I expect 50% of reflection on 2 and 50% of transmission on 3.
+        # For each of these ports, I expect 50% of energy in each polarization.
+        # Plus xy becomes x(-z) when reflected,
+        # So, 25% in 2x, 25 in 2z, 25 in 3x, 25 in 3y.
+        a = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        e = np.array([0, 0, 0, .25, 0, .25, .25, .25, 0, 0, 0, 0])
+        b = np.abs((G.dot(a))) ** 2  # Work in power.
+        print e
+        print b
+        print np.abs(b - e)
+        self.assertTrue(np.allclose(b, e, 0, 1.e-2))
 
 
 if __name__ == "__main__":
