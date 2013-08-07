68f0ef498be4d66bc116e2a10cb1371bf46a715f
Niriel <daneel_libre@yahoo.fr>
2013-08-07 14:13:39 +0200

    ISSTT paper, simplifications we make.

--- papers/isstt/hipe/sw_vs_noise.py
+++ papers/isstt/hipe/sw_vs_noise.py
@@ -1,5 +1,30 @@
-p = obs[0].product.refs["level2"].product.refs["WBS-H-USB"].product.refs["box_001"].product
+QUERY_RESULT = ProductStorage([PoolManager.getPool('hifi-aux'), PoolManager.getPool('pipeline-out'), PoolManager.getPool('standard'), PoolManager.getPool('hifi-cal'), PoolManager.getPool('hsa')]).select(herschel.ia.pal.query.MetaQuery(herschel.ia.obs.ObservationContext, "p", "(p.meta.containsKey(\"obsid\") and p.meta[\"obsid\"].value==1342190892L)"))
+obs = QUERY_RESULT[0]
+
+p = obs.product.refs["level2"].product.refs["WBS-H-USB"].product.refs["box_001"].product
 keys = p.keySet()
+
+for key in keys:
+    ds = p[key]
+    lo = ds.meta["loFrequency"].value
+    print lo
+    xs = []
+    ys = []
+    for sb_id in range(1, 5):
+        x = ds.getWave(sb_id)
+        y = ds.getFlux(sb_id)
+        assert len(x) == len(y)
+        xs.extend(x)
+        ys.extend(y)
+    xss = map(str, xs)
+    yss = map(str, ys)
+    line_x = " ".join(xss) + "\n"
+    line_y = " ".join(yss) + "\n"
+    filename = "%09.4f.csv" % lo
+    f = open(filename, 'w')
+    f.writelines([line_x, line_y])
+    f.close()
+
 maxes = []
 baselines = []
 noises = []
