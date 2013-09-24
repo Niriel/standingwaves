f8957b75ae5627a82736669c22344164fce6359e
Niriel <daneel_libre@yahoo.fr>
2013-09-24 11:44:32 +0200

    Version sent to IEEE.

--- code/isstt/load_data.py
+++ code/isstt/load_data.py
@@ -64,8 +64,8 @@ with my_style.latex(my_style.WIDTH_IEEE_COLUMN):
     my_style.pretty(1)
     ax_profile = plt.subplot(1, 1, 1)
     los = []
-    maxes = []
-    noises = []
+#     maxes = []
+#     noises = []
     color_id = 0
     # Zoom on the top of the plot.
     ax_peak = zoomed_inset_axes(ax_profile, 4, loc=2)
@@ -77,17 +77,21 @@ with my_style.latex(my_style.WIDTH_IEEE_COLUMN):
         y = y[to_keep]
         if len(x) <= 1:
             continue
-        baseline = np.median(y)
+        before = np.logical_and(x > 1036.50, x < 1037.00)
+        after = np.logical_and(x > 1037.30, x < 1037.80)
+        noline = np.logical_or(before, after)
+        baseline = np.average(y[noline])
+        no_baseline = y - baseline
         continuum_idx = y - baseline < .5
         los.append(lo)
-        maxes.append(np.max(smooth(y - baseline)))
-        noises.append(np.std(y[continuum_idx]))
-        ax_profile.plot(x, y - baseline)
-        ax_peak.plot(x, y - baseline)
-        ax_base.plot(x, y - baseline)
-    xmin = 1037.0
-    xmax = 1037.2
-    ticks_nb = 5
+#         maxes.append(np.max(smooth(no_baseline)))
+#         noises.append(np.std(y[continuum_idx]))
+        ax_profile.plot(x, no_baseline)
+        ax_peak.plot(x, no_baseline)
+        ax_base.plot(x, no_baseline)
+    xmin = 1037.00
+    xmax = 1037.25
+    ticks_nb = 6
     ticks_pos = np.linspace(xmin, xmax, ticks_nb)
     ticks_lbl = ['%07.2f' % tick_pos for tick_pos in ticks_pos]
     ticks_lbl = map(my_style.latexM, ticks_lbl)
@@ -104,12 +108,12 @@ with my_style.latex(my_style.WIDTH_IEEE_COLUMN):
     ax_peak.set_xticks([])
     ax_peak.set_yticks([])
     mark_inset(ax_profile, ax_peak, loc1=2, loc2=4, fc="none", ec="0.5")
-    x1, x2, y1, y2 = 1037.005, 1037.020, -.4, .6
+    x1, x2, y1, y2 = 1037.005, 1037.020, -1.0, 1.0
     ax_base.set_xlim(x1, x2)
     ax_base.set_ylim(y1, y2)
     ax_base.set_xticks([])
     ax_base.set_yticks([])
-    mark_inset(ax_profile, ax_base, loc1=3, loc2=1, fc="none", ec="0.5")
+    mark_inset(ax_profile, ax_base, loc1=2, loc2=3, fc="none", ec="0.5")
 #     ax_profile.title(my_style.latexT("Line profile"))
 
     # Plot showing the scatter.
