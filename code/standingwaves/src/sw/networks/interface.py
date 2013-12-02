89d65adb7e893a25f3c303e45e1cbbfc16b7134c
Niriel <daneel_libre@yahoo.fr>
2013-12-02 16:38:51 +0100

    Corrected rotations of thin films and interfaces.

--- code/standingwaves/src/sw/networks/interface.py
+++ code/standingwaves/src/sw/networks/interface.py
@@ -1,3 +1,4 @@
+
 """
 .. module:: sw.networks.interface
    :platform: Unix, Windows
@@ -9,6 +10,18 @@
 
 import numpy as np
 import sw.geometry as geo
+import collections
+
+InterfaceGeometry = collections.namedtuple("InterfaceGeometry",
+                                           "theta_a theta_b "
+                                           "area_compensation_a "
+                                           "area_compensation_b "
+                                           "P S "
+                                           "R12 R13 "
+                                           "R21 R24 "
+                                           "R31 R34 "
+                                           "R42 R43 "
+                                           "k2 k3 k4")
 
 def InterfaceNormal(na, nb):
     ra, ta = geo.FresnelNormal(na, nb)
@@ -17,47 +30,87 @@ def InterfaceNormal(na, nb):
     return np.array(np.bmat([[ra * I, tb * I],
                              [ta * I, rb * I]]))
 
-def InterfaceOblique(na, nb, n, k1):
-    theta_a = geo.ComputeAngleBetween(n, k1)
-    theta_b = geo.Snell(na, nb, theta_a)
+def _InterfaceGeometry(na, nb, n, k1):
     u = geo.ComputeIncidencePlaneNormal(n, k1)
-    a12 = 2 * theta_a - geo.TAU / 2
-    a13 = theta_a - theta_b
-    a21 = -a12
-    a31 = -a13
-    a34 = 2 * theta_b - geo.TAU / 2
+    # Because of the way we made u, theta_a is always going to be positive.
+    # So instead of this:
+    #     theta_a = geo.ComputeSignedAngleBetween(u, n, k1)
+    # We can just use the sign-less version.
+    theta_a = geo.ComputeAngleBetween(n, k1)
+    if not (0 <= theta_a % geo.TAU <= geo.TAU / 4):
+        raise ValueError(u"Wrong side of the surface.")
+
+    theta_b = geo.Snell(na, nb, theta_a)
+    # Incident field decomposition.
+    P, S = geo.MakeParaPerpDecompositionMatrices(u)
+
+    # The steeper the angle, the smaller the cross section of a finite-size
+    # beam. Going from low n to high n increases the angle. It also yields t >
+    # 1. Energy is conserved because there is more field in a smaller region.
+    # With infinite plane wave, it's a matter of density. Anyway, this is why we
+    # have these parameters that compensate for the change of area after a
+    # transmission.
+    area_compensation_b = np.sqrt(np.cos(theta_a) / np.cos(theta_b))
+    area_compensation_a = np.sqrt(np.cos(theta_b) / np.cos(theta_a))
+    a21 = -2 * theta_a
+    a31 = theta_b - theta_a
     a41 = -theta_a - theta_b
-    a43 = -a34
-    R12 = geo.RotationAroundAxisMatrix(u, a12)
-    R13 = geo.RotationAroundAxisMatrix(u, a13)
+    a43 = -2 * theta_b
+    R12 = geo.RotationAroundAxisMatrix(u, -a21)
+    R13 = geo.RotationAroundAxisMatrix(u, -a31)
     R21 = geo.RotationAroundAxisMatrix(u, a21)
     R31 = geo.RotationAroundAxisMatrix(u, a31)
-    R34 = geo.RotationAroundAxisMatrix(u, a34)
+    R34 = geo.RotationAroundAxisMatrix(u, -a43)
     R41 = geo.RotationAroundAxisMatrix(u, a41)
     R43 = geo.RotationAroundAxisMatrix(u, a43)
     R24 = R31
     R42 = R13
-    P, S = geo.MakeParaPerpDecompositionMatrices(u)
-    rpa, tpa, rsa, tsa = geo.FresnelOblique(na, nb, theta_a)
-    rpb, tpb, rsb, tsb = geo.FresnelOblique(nb, na, theta_b)
-    ra = rpa * P + rsa * S
-    rb = rpb * P + rsb * S
-    ta = tpa * P + tsa * S
-    tb = tpb * P + tsb * S
-    S12 = R12.dot(ra)
-    S13 = R13.dot(tb)
-    S21 = R21.dot(ra)
-    S24 = R24.dot(tb)
-    S31 = R31.dot(ta)
-    S34 = R34.dot(rb)
-    S42 = R42.dot(ta)
-    S43 = R43.dot(rb)
+    k2 = -R21.dot(k1)
+    k3 = R31.dot(k1)
+    k4 = R41.dot(k1)
+    return InterfaceGeometry(theta_a=theta_a, theta_b=theta_b,
+                             area_compensation_a=area_compensation_a,
+                             area_compensation_b=area_compensation_b,
+                             P=P, S=S,
+                             R12=R12, R13=R13, R21=R21, R24=R24,
+                             R31=R31, R34=R34, R42=R42, R43=R43,
+                             k2=k2, k3=k3, k4=k4)
+#     return ((theta_a, theta_b),
+#             (area_compensation_a, area_compensation_b),
+#             (P, S),
+#             (R12, R13, R21, R31, R34, R43, R24, R42),
+#             (k2, k3, k4))
+
+def InterfaceOblique(na, nb, n, k1):
+    ig = _InterfaceGeometry(na, nb, n, k1)
+#     theta_a, theta_b = intgeo.theta_a, intgeo.theta_b
+#     area_compensation_a, area_compensation_b = intgeo[1]
+#     P, S = intgeo[2]
+#     R12, R13, R21, R31, R34, R43, R24, R42 = intgeo[3]
+#     k2, k3, k4 = intgeo[4]
+    #
+    rpa, tpa, rsa, tsa = geo.FresnelOblique(na, nb, ig.theta_a)
+    rpb, tpb, rsb, tsb = geo.FresnelOblique(nb, na, ig.theta_b)
+    tpa *= ig.area_compensation_a
+    tsa *= ig.area_compensation_a
+    tpb *= ig.area_compensation_b
+    tsb *= ig.area_compensation_b
+    ra = rpa * ig.P + rsa * ig.S
+    rb = rpb * ig.P + rsb * ig.S
+    ta = tpa * ig.P + tsa * ig.S
+    tb = tpb * ig.P + tsb * ig.S
+    S12 = ig.R12.dot(ra)
+    S13 = ig.R13.dot(tb)
+    S21 = ig.R21.dot(ra)
+    S24 = ig.R24.dot(tb)
+    S31 = ig.R31.dot(ta)
+    S34 = ig.R34.dot(rb)
+    S42 = ig.R42.dot(ta)
+    S43 = ig.R43.dot(rb)
     ZZZ = np.zeros((3, 3), dtype=complex)
     S = np.array(np.bmat([[ZZZ, S12, S13, ZZZ],
                           [S21, ZZZ, ZZZ, S24],
                           [S31, ZZZ, ZZZ, S34],
                           [ZZZ, S42, S43, ZZZ]]))
-    k2 = R21.dot(k1)
-    k3 = R31.dot(k1)
-    k4 = R41.dot(k1)
-    return S, k2, k3, k4
+
+    return S, ig.k2, ig.k3, ig.k4
--- code/standingwaves/src/sw/networks/thinfilm.py
+++ code/standingwaves/src/sw/networks/thinfilm.py
@@ -1,11 +1,18 @@
 import numpy as np
 import sw.geometry as geo
 from distance import ComputeSpaceGain
+import collections
+
+ThinFilmGeometry = collections.namedtuple("ThinFilmGeometry",
+                                           "theta_a "
+                                           "P S "
+                                           "R12 R21 R34 R43 "
+                                           "k2 k3 k4")
 
 def _ThinFilmRT(n1, n2, thickness, frequency, angle1):
     angle2 = geo.Snell(n1, n2, angle1)  # Angle inside the film.
-    rp12, tp12, rs12, ts12 = geo.Fresnel(n1, n2, angle1)
-    rp21, tp21, rs21, ts21 = geo.Fresnel(n2, n1, angle2)
+    rp12, tp12, rs12, ts12 = geo.FresnelOblique(n1, n2, angle1)
+    rp21, tp21, rs21, ts21 = geo.FresnelOblique(n2, n1, angle2)
     path_length = thickness / np.cos(angle2)
     a = ComputeSpaceGain(n2, path_length, frequency)
     # A one way transmission has a gain of t12 * a * t21.  Then, on top of that,
@@ -14,7 +21,7 @@ def _ThinFilmRT(n1, n2, thickness, frequency, angle1):
     # series of ratio q is 1 / (1-q).  This modulates the original one way
     # transmission.
     tp = tp12 * tp21 * a / (1 - (rp21 * a) ** 2)
-    ts = ts12 * ts21 * a / (1 - (rp21 * a) ** 2)
+    ts = ts12 * ts21 * a / (1 - (rs21 * a) ** 2)
     # The reflection starts a bit differently, but then, the same round trips
     # happen and we can reuse tp and ts.  Indeed, it's as if, instead of
     # reflecting from one port, we were transmitting from the other.
@@ -33,54 +40,96 @@ def _ThinFilmRT(n1, n2, thickness, frequency, angle1):
     ts *= space_neg
     return rp, tp, rs, ts
 
-def _ThinFilmGeometry(tb, attitude, k1):
-    rdir, _ = tb.rot(*attitude)
-    surface_normal = rdir.dot(tb.rest)
-    angle1 = geo.ComputeAngleBetween(surface_normal, k1)
-    assert 0 <= angle1 % geo.TAU <= geo.TAU / 4, "Wrong side of the surface."
+def _ThinFilmGeometry(n, k1):
+    u = geo.ComputeIncidencePlaneNormal(n, k1)
+    # Because of the way we made u, theta_a is always going to be positive.
+    # So instead of this:
+    #     theta_a = geo.ComputeSignedAngleBetween(u, n, k1)
+    # We can just use the sign-less version.
+    theta_a = geo.ComputeAngleBetween(n, k1)
+    if not (0 <= theta_a % geo.TAU <= geo.TAU / 4):
+        raise ValueError(u"Wrong side of the surface.")
     # Incident field decomposition.
-    ipn = geo.ComputeIncidencePlaneNormal(surface_normal, k1)
-    P, S = geo.MakeParaPerpDecompositionMatrices(ipn)
+    P, S = geo.MakeParaPerpDecompositionMatrices(u)
     # Propagation and field rotations.
-    ipn_h, ipn_p = tb.hp(ipn)
-    roll21 = -2 * angle1
-    roll41 = -2 * angle1
-    roll43 = -2 * angle1
-    R12, _ = tb.around(ipn_h, ipn_p, -roll21)
-    R21, _ = tb.around(ipn_h, ipn_p, roll21)
-    R34, _ = tb.around(ipn_h, ipn_p, -roll43)
-    R43, _ = tb.around(ipn_h, ipn_p, roll43)
-    R41, _ = tb.around(ipn_h, ipn_p, roll41)
+    # Ports 1 and 3 have the same direction,
+    # ports 2 and 4 have the same direction.
+    # Going from 1/3 to 2/4 removes 2*theta_a
+    a = 2 * theta_a
+    Rpos = geo.RotationAroundAxisMatrix(u, a)
+    Rneg = geo.RotationAroundAxisMatrix(u, -a)
+    R12 = Rpos
+    R21 = Rneg
+    R34 = Rpos
+    R41 = Rneg
+    R43 = Rneg
     k2 = -R21.dot(k1)
     k3 = k1
     k4 = R41.dot(k1)
-    return angle1, P, S, R12, R21, R34, R43, k2, k3, k4
+    return ThinFilmGeometry(theta_a=theta_a,
+                            P=P, S=S,
+                            R12=R12, R21=R21, R34=R34, R43=R43,
+                            k2=k2, k3=k3, k4=k4)
 
-def ThinFilm(n1, n2, thickness, frequency, tb, attitude, k1):
+def ThinFilmOblique(n1, n2, thickness, frequency, tb, att, k1):
     """Thin film of material n2 at non-normal incidence.  4 ports.
 
     """
-    angle1, P, S, R12, R21, R34, R43, k2, k3, k4 = _ThinFilmGeometry(tb, attitude, k1)
-    rp, tp, rs, ts = _ThinFilmRT(n1, n2, thickness, frequency, angle1)
-    # Transmission: no rotation.
-    S13 = S24 = S31 = S42 = tp * P + ts * S
+    n = tb.rot(*att).dot(tb.rest)
+    tg = _ThinFilmGeometry(n, k1)
+    rp, tp, rs, ts = _ThinFilmRT(n1, n2, thickness, frequency, tg.theta_a)
     # Reflections: rotations.
-    Sr = rp * P + rs * S
-    S12 = R12.dot(Sr)
-    S21 = R21.dot(Sr)
-    S34 = R34.dot(Sr)
-    S43 = R43.dot(Sr)
+    Sr = rp * tg.P + rs * tg.S
+    S12 = tg.R12.dot(Sr)
+    S21 = tg.R21.dot(Sr)
+    S34 = tg.R34.dot(Sr)
+    S43 = tg.R43.dot(Sr)
+    # Transmission: no rotation.
+    S13 = S24 = S31 = S42 = tp * tg.P + ts * tg.S
     # Impossible paths.
-    S11 = S14 = S22 = S23 = S32 = S33 = S41 = S44 = np.zeros((3, 3))
-    S = np.array(np.bmat([[S11, S12, S13, S14],
-                          [S21, S22, S23, S24],
-                          [S31, S32, S33, S34],
-                          [S41, S42, S43, S44]]))
-    return S, k2, k3, k4
+    ZZZ = np.zeros((3, 3), dtype=complex)
+    S = np.array(np.bmat([[ZZZ, S12, S13, ZZZ],
+                          [S21, ZZZ, ZZZ, S24],
+                          [S31, ZZZ, ZZZ, S34],
+                          [ZZZ, S42, S43, ZZZ]]))
+    return S, tg.k2, tg.k3, tg.k4
 
-def ThinFilmNormal(n1, n2, thickness, frequency, tb, attitude):
+def ThinFilmNormal(n1, n2, n3, thickness, frequency, tb, attitude):
     """Thin film of material n2 at normal incidence.  2 ports.
 
     """
-#     rp12, tp12, rs12, ts12 = geo.Fresnel(n1, n2, 0)
-#     rp21, tp21, rs21, ts21 = geo.Fresnel(n2, n1, 0)
+    r12, t12 = geo.FresnelNormal(n1, n2)
+    r21, t21 = geo.FresnelNormal(n2, n1)
+    r32, t32 = geo.FresnelNormal(n3, n2)
+    r23, t23 = geo.FresnelNormal(n2, n3)
+    a2 = ComputeSpaceGain(n2, thickness, frequency)
+    frac = a2 / (1 - a2 * a2 * r21 * r23)
+    r13 = r12 + t12 * t21 * a2 * r23 * frac
+    r31 = r32 + t32 * t23 * a2 * r21 * frac
+    t13 = t12 * t23 * frac
+    t31 = t32 * t21 * frac
+    # Where the film is, there is no air.
+    a1 = ComputeSpaceGain(n1, -thickness / 2, frequency)
+    a3 = ComputeSpaceGain(n3, -thickness / 2, frequency)
+    r13 = a1 * a1 * r13
+    r31 = a3 * a3 * r31
+    t13 = a1 * a3 * t13
+    t31 = a1 * a3 * t31
+    # Jones matrices.
+    I = np.identity(3, dtype=complex)  # Last column meaningless for transverse.
+    S11 = r13 * I
+    S12 = t31 * I
+    S21 = t13 * I
+    S22 = r31 * I
+    # Attitude corrections.
+    R = tb.rot(*attitude)
+    Rt = R.T
+    S11 = R.dot(S11).dot(Rt)
+    S12 = R.dot(S12).dot(Rt)
+    S21 = R.dot(S21).dot(Rt)
+    S22 = R.dot(S22).dot(Rt)
+    #
+    S = np.array(np.bmat([[S11, S12],
+                          [S21, S22]]))
+    return S
+
