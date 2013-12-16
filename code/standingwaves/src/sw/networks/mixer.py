e8086df8ae427fdc2bac7ab8d3728a01aae4d178
Niriel <daneel_libre@yahoo.fr>
2013-12-16 16:51:46 +0100

    Over simplified mixer model, useful though.

---
+++ code/standingwaves/src/sw/networks/mixer.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def Mixer(rx, ry, tx, ty, tb, attitude):
+    Rd = tb.rot(*attitude)
+    Ri = Rd.T
+    S11 = np.array([[rx, 0, 0],
+                    [0, ry, 0],
+                    [0, 0, 0]], dtype=complex)
+    S12 = np.zeros((3, 3))
+    S21 = np.array([[tx, 0, 0],
+                    [0, ty, 0],
+                    [0, 0, 0]], dtype=complex)
+    S22 = np.zeros((3, 3))
+    S11 = Rd.dot(S11).dot(Ri)
+    S21 = Rd.dot(S21).dot(Ri)
+    return np.array(np.bmat([[S11, S12],
+                             [S21, S22]]))
